cdbf2b94ecb524903a25d68ab187f8151b0d4abf
jammy-sdgku <james.jones@sdgku.edu>
2025-11-09 21:45:24 +0000

    eb configs

--- .platform/hooks/prebuild/01_fix_procfile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This runs BEFORE EB tries to generate a Procfile
-cd /var/app/staging
-echo "web: gunicorn --bind 127.0.0.1:8000 --workers 3 --threads 2 --chdir Django_Checkbook Django_Checkbook.wsgi:application" > Procfile
