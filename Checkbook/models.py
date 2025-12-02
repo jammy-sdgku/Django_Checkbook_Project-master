277f4549097af58037ad40ef0146cd217f32b9eb
jammy-sdgku <james.jones@sdgku.edu>
2025-12-02 05:08:22 +0000

    added ctaegory field to database

--- Checkbook/models.py
+++ Checkbook/models.py
@@ -13,12 +13,22 @@ class Account(models.Model):
 
 TransactionTypes = [('Deposit', 'Deposit'), ('Withdrawal', 'Withdrawal')]
 
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    Categories = models.Manager()
+
+    def __str__(self):
+        return self.name
+    
+CategoryTypes = [('House', 'House'), ('Work', 'Work'), ('Education', 'Education'), ('Entertainment/Dining', 'Entertainment/Dining'), ('Auto', 'Auto'), ('Bank', 'Bank'), ('Transfer', 'Transfer'), ('Misc', 'Misc')]
 
 class Transaction(models.Model):
     date = models.DateField()
     type = models.CharField(max_length=10, choices=TransactionTypes)
     to_from = models.CharField(max_length=100, default='none')
     amount = models.DecimalField(max_digits=15, decimal_places=2)
+    category = models.ForeignKey(category, on_delete=models.CASCADE, null=True, blank=True)
     description = models.CharField(max_length=100)
     cleared = models.BooleanField(default=False)
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
