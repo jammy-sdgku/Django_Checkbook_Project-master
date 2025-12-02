e5718aacf81bbbaeae87799258aabd537a99aa52
jammy-sdgku <james.jones@sdgku.edu>
2025-12-02 17:58:21 +0000

    Fix STATIC files errors

--- Django_Checkbook/settings.py
+++ Django_Checkbook/settings.py
@@ -141,14 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Change from 'staticfiles' to 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]   # add project-level static folder
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
